d7d854c4d94ae74fa1a48197d62846d3f7d8bded
Arie van der Poel <arie@mediasuite.co.nz>
2021-05-30 17:56:02 +1200

    Working pong

--- pong.js
+++ pong.js
@@ -1,9 +1,14 @@
 // requestAnimationFrame calls a callback at 60fps. If the tab isn't active it will stop making calls until it becomes active again
 
-const animate = window.requestAnimationFrame ||
+const animate = 
+  window.requestAnimationFrame ||
   window.webkitRequestAnimationFrame ||
   window.mozRequestAnimationFrame ||
-  function(callback) { window.setTimeout(callback, 1000/60) }
+  function(callback) { 
+    window.setTimeout(callback, 1000/60) 
+  }
+
+// Could also use getElementByID
 
 const canvas = document.createElement('canvas')
 const width = 400
@@ -29,13 +34,17 @@ function step() {
   animate(step)
 }
 
+// Pass computer and player paddle to the ball.update function
+
 function update() {
-  ball.update();
+  player.update()
+  computer.update(ball)
+  ball.update(player.paddle, computer.paddle)
 }
 
-var player = new Player();
-var computer = new Computer();
-var ball = new Ball(200, 300);
+let player = new Player();
+let computer = new Computer();
+let ball = new Ball(200, 300);
 
 function render() {
   context.fillStyle = "#000000";
@@ -87,16 +96,111 @@ Paddle.prototype.render = function() {
   context.fillRect(this.x, this.y, this.width, this.height);
 }
 
+Player.prototype.update = function() {
+  for(var key in keysDown) {
+    var value = Number(key);
+    if(value == 37) { // left arrow
+      this.paddle.move(-4, 0);
+    } else if (value == 39) { // right arrow
+      this.paddle.move(4, 0);
+    } else {
+      this.paddle.move(0, 0);
+    }
+  }
+};
+
+Paddle.prototype.move = function(x, y) {
+  this.x += x;
+  this.y += y;
+  this.x_speed = x;
+  this.y_speed = y;
+  if(this.x < 0) { // all the way to the left
+    this.x = 0;
+    this.x_speed = 0;
+  } else if (this.x + this.width > 400) { // all the way to the right
+    this.x = 400 - this.width;
+    this.x_speed = 0;
+  }
+}
+
+// context.arc? 
+
 Ball.prototype.render = function() {
   context.beginPath();
   context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
   context.fillStyle = "#FFFFFF";
   context.fill();
-};
+}
 
 // Cleverly sets the ball to be animated by updating it's x/y value each fps
 
-Ball.prototype.update = function() {
+// The collision detection is happening is Axis Aligned Bounding Boxes or AABBs
+
+// If the paddle is moving when it hits the ball, the x_speed is added to the ball. This will cause it to move faster or slower depending on the direction of the ball and the direction of the paddle.
+
+Ball.prototype.update = function(paddle1, paddle2) {
   this.x += this.x_speed;
   this.y += this.y_speed;
+  let top_x = this.x - 5;
+  let top_y = this.y - 5;
+  let bottom_x = this.x + 5;
+  let bottom_y = this.y + 5;
+
+  if(this.x - 5 < 0) { // hitting the left wall
+    this.x = 5;
+    this.x_speed = -this.x_speed;
+  } else if(this.x + 5 > 400) { // hitting the right wall
+    this.x = 395;
+    this.x_speed = -this.x_speed;
+  }
+
+  if(this.y < 0 || this.y > 600) { // a point was scored
+    this.x_speed = 0;
+    this.y_speed = 3;
+    this.x = 200;
+    this.y = 300;
+  }
+
+  if(top_y > 300) {
+    if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
+      // hit the player's paddle
+      this.y_speed = -3;
+      this.x_speed += (paddle1.x_speed / 2);
+      this.y += this.y_speed;
+    }
+  } else {
+    if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
+      // hit the computer's paddle
+      this.y_speed = 3;
+      this.x_speed += (paddle2.x_speed / 2);
+      this.y += this.y_speed;
+    }
+  }
+}
+
+Computer.prototype.update = function(ball) {
+  var x_pos = ball.x;
+  var diff = -((this.paddle.x + (this.paddle.width / 2)) - x_pos);
+  if(diff < 0 && diff < -4) { // max speed left
+    diff = -5;
+  } else if(diff > 0 && diff > 4) { // max speed right
+    diff = 5;
+  }
+  this.paddle.move(diff, 0);
+  if(this.paddle.x < 0) {
+    this.paddle.x = 0;
+  } else if (this.paddle.x + this.paddle.width > 400) {
+    this.paddle.x = 400 - this.paddle.width;
+  }
 };
+
+let keysDown = {};
+
+window.addEventListener("keydown", function(event) {
+  keysDown[event.keyCode] = true;
+});
+
+window.addEventListener("keyup", function(event) {
+  delete keysDown[event.keyCode];
+});
+
